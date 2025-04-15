006d1b0cf1ca01c34d76c6d0c0a59b97185ad20e
admin <admin@edelweis1254.krvostok.ru>
2025-04-15 17:02:11 +0300

    Change task.go

--- handlers/task.go
+++ handlers/task.go
@@ -1,1 +1,103 @@
 package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+// Task представляет одну задачу в списке дел
+type Task struct {
+	ID    int    `json:"id"`    // Уникальный идентификатор задачи
+	Title string `json:"title"` // Название задачи
+	Done  bool   `json:"done"`  // Статус выполнения задачи
+}
+
+var tasks []Task  // Хранилище задач (в памяти)
+var currentID = 1 // Глобальный счетчик для генерации уникальных ID
+
+// GetTasks возвращает все задачи
+func GetTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
+
+// GetTaskByID возвращает задачу по ID
+func GetTaskByID(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // Получаем параметры из URL
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid task ID", http.StatusBadRequest)
+		return
+	}
+	for _, task := range tasks {
+		if task.ID == id {
+			json.NewEncoder(w).Encode(task)
+			return
+		}
+	}
+	http.Error(w, "Task not found", http.StatusNotFound)
+}
+
+// CreateTask создает новую задачу
+func CreateTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var newTask Task
+	err := json.NewDecoder(r.Body).Decode(&newTask)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+	newTask.ID = currentID
+	currentID++
+	tasks = append(tasks, newTask)
+	json.NewEncoder(w).Encode(newTask)
+}
+
+// UpdateTask обновляет существующую задачу
+func UpdateTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid task ID", http.StatusBadRequest)
+		return
+	}
+	var updatedTask Task
+	err = json.NewDecoder(r.Body).Decode(&updatedTask)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+	for i, task := range tasks {
+		if task.ID == id {
+			updatedTask.ID = id
+			tasks[i] = updatedTask
+			json.NewEncoder(w).Encode(updatedTask)
+			return
+		}
+	}
+	http.Error(w, "Task not found", http.StatusNotFound)
+}
+
+// DeleteTask удаляет задачу по ID
+func DeleteTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid task ID", http.StatusBadRequest)
+		return
+	}
+	for i, task := range tasks {
+		if task.ID == id {
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+	}
+	http.Error(w, "Task not found", http.StatusNotFound)
+}
